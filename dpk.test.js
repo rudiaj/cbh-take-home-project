5dbf778701a562f04e36b76514845f02a524fe87
Rudi Aj <raj@dickeys.com>
2023-05-09 21:08:39 +0200

    fix: wording in test cases

--- dpk.test.js
+++ dpk.test.js
@@ -2,19 +2,19 @@ const { deterministicPartitionKey } = require("./dpk");
 
 describe("deterministicPartitionKey", () => {
   it("Returns the literal '0' when given no input", () => {
-    const trivialKey = deterministicPartitionKey();
-    expect(trivialKey).toBe("0");
+    const result = deterministicPartitionKey();
+    expect(result).toBe("0");
   });
 
   it("Returns the hash when given empty object", () => {
-    const trivialKey = deterministicPartitionKey({});
-    expect(trivialKey).toBe(
+    const result = deterministicPartitionKey({});
+    expect(result).toBe(
       "c1802e6b9670927ebfddb7f67b3824642237361f07db35526c42c555ffd2dbe74156c366e1550ef8c0508a6cc796409a7194a59bba4d300a6182b483d315a862"
     );
   });
 
-  it("Returns the literal '1' when given no input", () => {
-    const trivialKey = deterministicPartitionKey({ partitionKey: "257" });
-    expect(trivialKey).toBe("257");
+  it("Returns the literal '257' when correct event", () => {
+    const result = deterministicPartitionKey({ partitionKey: "257" });
+    expect(result).toBe("257");
   });
 });
